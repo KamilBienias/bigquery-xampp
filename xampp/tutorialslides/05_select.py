ae97fa2f347c3a38fa1d27879786c735e59396b8
KamilBienias <kamil.bienias.math@gmail.com>
2021-03-07 00:39:41 +0100

    moja funkcja show_table()

--- xampp/tutorialslides/05_select.py
+++ xampp/tutorialslides/05_select.py
@@ -10,22 +10,43 @@ cursor = connection.cursor()
 # some other statements  with the help of cursor
 print("Connection successful")
 
-print()
-print("Tabela w bazie =")
-# pokazuje nazwy kolumn w tabeli
-columns_names_query = "SHOW COLUMNS FROM Artists;"
-cursor.execute(columns_names_query)
-rows = cursor.fetchall()
-for row in rows:
-   print(row[0], end=" ")
-print()
-# zapytanie do wyswietlenia wszystkich wierszy i kolumn
-retrive_query = "SELECT * FROM Artists;"
-# wyswietla wiersz po wierszu
-cursor.execute(retrive_query)
-rows = cursor.fetchall()
-for row in rows:
-   print(row)
+print("Moja funkcja")
+
+
+def show_table(base_and_table_name):
+   # pokazuje nazwy kolumn w tabeli
+   print("Tabela " + base_and_table_name + " w bazie =")
+   columns_names_query = "SHOW COLUMNS FROM " + base_and_table_name + ";"
+   cursor.execute(columns_names_query)
+   rows = cursor.fetchall()
+   for row in rows:
+      print(row[0], end=" ")
+   print()
+   retrive_query = "SELECT * FROM " + base_and_table_name + ";"
+   # wyswietla wiersz po wierszu
+   cursor.execute(retrive_query)
+   rows = cursor.fetchall()
+   for row in rows:
+      print(row)
+
+show_table("bigquery_data_types.Artists")
+
+# print()
+# print("Tabela w bazie =")
+# # pokazuje nazwy kolumn w tabeli
+# columns_names_query = "SHOW COLUMNS FROM Artists;"
+# cursor.execute(columns_names_query)
+# rows = cursor.fetchall()
+# for row in rows:
+#    print(row[0], end=" ")
+# print()
+# # zapytanie do wyswietlenia wszystkich wierszy i kolumn
+# retrive_query = "SELECT * FROM Artists;"
+# # wyswietla wiersz po wierszu
+# cursor.execute(retrive_query)
+# rows = cursor.fetchall()
+# for row in rows:
+#    print(row)
 
 
 #commiting the connection then closing it.
