42990663ed527b7f257d6a095982a77517048bc3
KamilBienias <kamil.bienias.math@gmail.com>
2021-03-07 00:42:01 +0100

    moja funkcja show_table()

--- xampp/tutorialslides/05_select.py
+++ xampp/tutorialslides/05_select.py
@@ -11,8 +11,6 @@ cursor = connection.cursor()
 print("Connection successful")
 
 print("Moja funkcja")
-
-
 def show_table(base_and_table_name):
    # pokazuje nazwy kolumn w tabeli
    print("Tabela " + base_and_table_name + " w bazie =")
@@ -27,29 +25,15 @@ def show_table(base_and_table_name):
    cursor.execute(retrive_query)
    rows = cursor.fetchall()
    for row in rows:
-      print(row)
+       print(row)
 
-show_table("bigquery_data_types.Artists")
+try:
+   show_table("bigquery_data_types.Artists")
 
-# print()
-# print("Tabela w bazie =")
-# # pokazuje nazwy kolumn w tabeli
-# columns_names_query = "SHOW COLUMNS FROM Artists;"
-# cursor.execute(columns_names_query)
-# rows = cursor.fetchall()
-# for row in rows:
-#    print(row[0], end=" ")
-# print()
-# # zapytanie do wyswietlenia wszystkich wierszy i kolumn
-# retrive_query = "SELECT * FROM Artists;"
-# # wyswietla wiersz po wierszu
-# cursor.execute(retrive_query)
-# rows = cursor.fetchall()
-# for row in rows:
-#    print(row)
-
-
-#commiting the connection then closing it.
-connection.commit()
-connection.close()
-print("Connection closed")
+   #commiting the connection then closing it.
+   connection.commit()
+except Exception as e:
+   print(e)
+finally:
+   connection.close()
+   print("Connection closed")
